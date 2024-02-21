ca9824524941c3625b1295e3e2caa3334b4dc06a
Urielcont <Urielalvaro3@gmail.com>
2024-02-20 22:12:33 -0600

    Creacion campos de formularios

--- LoginFrontEnd/src/Pages/RegisterPage.jsx
+++ LoginFrontEnd/src/Pages/RegisterPage.jsx
@@ -1,6 +1,19 @@
+import {useForm} from 'react-hook-form'
+
 function RegisterPage(){
+    const {Register, handleSubmit}=useForm();
     return(
-        <div>Registrarse</div>
+        <div>
+            <form onSubmit={handleSubmit(values=>{
+                console.log(values)
+            })}>
+                <input type="text" {...Register("username",{required:true})} />
+                <input type="email" {...Register("email",{required:true})}/>
+                <input type="password" {...Register("password",{required:true})}/>
+
+                <button type='submit'>Registrarse</button>
+            </form>
+        </div>
     )
 }
 export default RegisterPage;
